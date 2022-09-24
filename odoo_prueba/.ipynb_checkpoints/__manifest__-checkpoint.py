75fb263470b37c1c6d39f253aafa78092c9aee52
ivan <odootest725@gmail.com>
2022-09-24 18:19:52 +0000

    [ADD] Modulocreado

---
+++ odoo_prueba/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo prueba',
+    
+    'summary':"""Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to manage Training:
+        -Courses
+        -Sessions
+        -Attendees
+    """,
+    
+    'author': 'Ivan',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends':['base'],
+    
+    'data':[
+       
+        
+    ],
+    
+    'demo':[
+        
+    ],
+    
+}
